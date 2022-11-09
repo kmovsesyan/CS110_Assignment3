f18012d328edbf825c44bd9df1ed0456cf135bcc
kmovsesyan <117852187+kmovsesyan@users.noreply.github.com>
2022-11-09 21:42:15 +0400

    Add files via upload

---
+++ rev.js
@@ -0,0 +1,9 @@
+let arr = [1,2,3];
+      let rev = [];
+      let i;
+      for(i = arr.length-1; i>=0; i--){
+         rev.push(arr[i]);
+      }
+      for(let i = 0; i<rev.length; i++){
+         console.log(rev[i] + " ");
+      }
